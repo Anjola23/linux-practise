0031cba590b7acf7491b0f9ea38f140c7890f305
Anjola23 <graceolakanmi0@gmail.com>
2024-01-16 17:33:25 +0100

    added one question into dsforstat

--- dsforstats.py
+++ dsforstats.py
@@ -5,6 +5,7 @@ from utils import probability
 from utils.testing import test_equal, test_close
 import utils.counting as C
 
+
 print("\nSOME PROBABILITY PROBLEMS\n")
 
 # Question 1
@@ -23,3 +24,11 @@ print("A: The probability is {}".format(p_3_heads))
 expected_p_3_heads = 0.1171875
 test_close(p_3_heads, expected_p_3_heads)
 print("")
+
+
+# Question 3
+print("Q: Find the probability of getting 3 heads when you toss 10 fair coins.")
+p_3_heads = C.combinations(10, 2) / 2**10
+print("A: The probability is {}".format(p_3_heads))
+expected_p_3_heads = 0.1171875
+test_close(p_3_heads, expected_p_3_heads)
